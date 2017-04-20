53a59daed58be984424cd81951c4e3b3fec6c069
panidaoum <panidaoum555@gmail.com>
2017-04-20 20:58:12 +0700

    edit

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Business Casual - Start Bootstrap Theme</title>
+    <title>About Beauty</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -32,8 +32,8 @@
 
 <body>
 
-    <div class="brand">Business Casual</div>
-    <div class="address-bar">3481 Melrose Place | Beverly Hills, CA 90210 | 123.456.7890</div>
+    <div class="brand">About Beauty</div>
+    <div class="address-bar">| Web592group22 |</div>
 
     <!-- Navigation -->
     <nav class="navbar navbar-default" role="navigation">
@@ -47,22 +47,25 @@
                     <span class="icon-bar"></span>
                 </button>
                 <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
-                <a class="navbar-brand" href="index.html">Business Casual</a>
+                <a class="navbar-brand" href="index.php">About Beauty</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li>
-                        <a href="index.html">Home</a>
+                        <a href="index.php">HOME</a>
                     </li>
                     <li>
-                        <a href="about.html">About</a>
+                        <a href="EYE SHADOW.html">EYE SHADOW</a>
                     </li>
                     <li>
-                        <a href="blog.html">Blog</a>
+                        <a href="BLUSH.html">BLUSH</a>
                     </li>
                     <li>
-                        <a href="contact.html">Contact</a>
+                        <a href="LIP.html">LIP</a>
+                    </li>
+					<li>
+                        <a href="CUSHION.html">CUSHION </a>
                     </li>
                 </ul>
             </div>
@@ -108,11 +111,11 @@
                     <h2 class="brand-before">
                         <small>Welcome to</small>
                     </h2>
-                    <h1 class="brand-name">Business Casual</h1>
+                    <h1 class="brand-name">About Beauty</h1>
                     <hr class="tagline-divider">
                     <h2>
                         <small>By
-                            <strong>Start Bootstrap</strong>
+                            <strong>Web592group22</strong>
                         </small>
                     </h2>
                 </div>
@@ -123,15 +126,16 @@
             <div class="box">
                 <div class="col-lg-12">
                     <hr>
-                    <h2 class="intro-text text-center">Build a website
-                        <strong>worth visiting</strong>
+                     <h2 class="intro-text text-center">ทัศนคติผู้หญิงที่มีต่อค่านิยมความงาม
+                       
                     </h2>
                     <hr>
                     <img class="img-responsive img-border img-left" src="img/intro-pic.jpg" alt="">
                     <hr class="visible-xs">
-                    <p>The boxes used in this template are nested inbetween a normal Bootstrap row and the start of your column layout. The boxes will be full-width boxes, so if you want to make them smaller then you will need to customize.</p>
-                    <p>A huge thanks to <a href="http://join.deathtothestockphoto.com/" target="_blank">Death to the Stock Photo</a> for allowing us to use the beautiful photos that make this template really come to life. When using this template, make sure your photos are decent. Also make sure that the file size on your photos is kept to a minumum to keep load times to a minimum.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat diam quis nisl vestibulum dignissim. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                    <p>เมื่อผู้หญิงถูกคาดหวังผ่านเรื่องราวของสื่อต่างๆ มากมาย โดยเฉพาะหญิงงามที่กำเนิดอยู่ในชาติตระกูลที่มีเกียรติและมียศสูงส่ง ก็ยิ่งมักจะถูกจินตนาการและคาดหวังว่าจะต้องเป็นคนที่สวย ทั้งรวยทรัพย์และความงาม ในอดีตความงามของสตรียังถูกใช้เป็นเครื่องบ่งบอกความหมายถึงฐานะและบารมีของสามี หากสามีคนใดที่มีภรรยางามก็ย่อมได้รับการกล่าวขานและชื่นชม เมื่อความสวยงามของผู้หญิงถูกคาดหวังอย่างมากมายภายใจสังคม ตั้งแต่อดีตสืบเนื่องมาจนถึงปัจจุบัน ส่งผลให้เกิดอาชีพต่างๆ ผุดขึ้นมามากมาย ตั้งแต่ ช่างเสริมความงาม ช่างทำผม ช่างทำเสื้อ และที่กำลังแพร่หลายอยู่ในปัจจุบันมากขึ้นก็คือการทำศัลยกรรมประเภทต่างๆ</p>
+                    <p>ทำให้ปฏิเสธไม่ได้ว่าผู้หญิงในสมัยนี้ที่มีหน้าตาสะสวยมักจะได้รับการยอมรับและเปิดกว้างในด้านต่างๆ มากกว่าคนที่หน้าตาขี้เหร่ ผู้คนส่วนมากในสังคมยังนิยมความสวยของผู้หญิงมากกว่าคุณค่าที่มาจากตัวตนจริงๆ หรือผู้หญิงที่มีความเก่งก็มิอาจที่จะสู้ทัดเทียมได้กับผู้หญิงที่สวยกว่าตนเอง นอกจากจะต้องขวนขวายอย่างหนักมากจึงจะสามารถไปยืน ณ จุดๆ เดียวกันกับพวกเธอที่มีใบหน้าอันสวยงามได้ สิ่งเหล่านี้จึงเปรียบเสมือนค่านิยมทางสังคม หรืออาจจะเป็นเรื่องของธรรมชาติที่สร้างสรรค์ให้มนุษย์เกิดการคัดเลือกทางสายพันธุ์ที่สมบูรณ์และเหมาะสมให้เหลือรอดเอาไว้ในสังคม และมันยังคงเป็นส่วนหนึ่งของจิตใต้สำนึกของเราที่ยากจะกลบเกลื่อนให้หายไปจากสังคม</p>
+                    <p>ดังนั้นผู้หญิงก็ยังคงต้องพยายามสร้างสรรค์บุคลิกของตนเองให้ดูดีและสมบูรณ์แบบอยู่เสมอไม่ว่าจะด้วยหนทางใดก็ตาม และเพราะค่านิยมที่ครอบงำความคิดเราไว้แต่แบบนี้ทำให้ผู้หญิงยุคใหม่หลายคนมองข้ามคุณค่าจากความงามภายในไปพร้อมกันด้วย แท้จริงแล้ว ยังมีความงามจากภายในหรือจากอุปนิสัยใจคอที่ดี อ่อนโยน อ่อนหวาน จิตใจดี มีเมตตาแบบนี้ก็นับเป็นความสวยงามที่สร้างเองได้จากภายใน และมันยังสะท้อนให้บุคลิกของผู้หญิงเรามีเสน่ห์และคุณค่าในตัวเองอย่างมากอีกด้วย</p>
+					<p>เพราะฉะนั้น เรายังสามารถปรับบุคลิกให้ตัวเองดูดีด้วยการออกกำลังกาย แต่งหน้า ทำผม แต่งตัวให้เป็นเพื่ออำพรางส่วนด้อยของตัวเอง เท่านี้ก็ทำให้ค่านิยมเหล่านั้นเป็นเรื่องที่คุณมองข้ามไปได้แล้ว สาวๆยุคใหม่ก็ย่อมใช้ชีวิตแบบที่เป็นตัวเองมากที่สุด</p>
                 </div>
             </div>
         </div>
@@ -140,13 +144,17 @@
             <div class="box">
                 <div class="col-lg-12">
                     <hr>
-                    <h2 class="intro-text text-center">Beautiful boxes
-                        <strong>to showcase your content</strong>
+                    <h2 class="intro-text text-center">ปัญหาเรื่องเครื่องสำอาง
+                       
                     </h2>
                     <hr>
-                    <p>Use as many boxes as you like, and put anything you want in them! They are great for just about anything, the sky's the limit!</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat diam quis nisl vestibulum dignissim. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-                </div>
+					<img class="img-responsive img-border img-left" src="img/intro-pic2.jpg" alt="">
+					  
+                    <p>ลักษณะของผิวแบ่งออกเป็นกลุ่มใหญ่ๆได้ 3 ชนิด คือผิวแห้ง ผิวมัน และผิวไม่เสมอ คือมีทั้งแห้งทั้งมัน ส่วนผิวชนิดไหนจะเกิดการแพ้ได้ง่ายกว่ากัน </p>
+                    <p>คำตอบคือทุกชนิดมีโอกาสจะแพ้หรือเกิดปัญหาจากการใช้เครื่องสำอางได้เท่าๆกัน ทั้งนี้ขึ้นอยู่กับความพอเหมาะพอสมของเครื่องสำอางกับผิว คือจะต้องรู้จักเลือกใช้เครื่องสำอางให้เหมาะกับผิว</p>
+					<p>ยกตัวอย่าง คนผิวมันมีสิวแล้วไปเลือกใช้ครีมล้างหน้าหรือรองพื้นที่เป็นมัน เครื่องใช้ไม่เข้ากับผิวก็จะยิ่งทำให้หน้ามันมากขึ้นเกิดการอุดตันของต่อมน้ำมันทำให้สิวกำเริบเป็นมากขึ้น</p>
+					<p>หรือคนผิวแห้งไปใช้น้ำยาประเภทล้างไขมัน เช่น น้ำยาเช็ดหน้าที่มีส่วนผสมของแอลกอฮอล์หรือใช้สบู่ล้างหน้าบ่อยๆก็ย่อมจะทำให้หน้ายิ่งแห้ง น้ำมันหล่อเลี้ยงที่มีอยู่น้อยถูกทำลายไปหมด เกิดหน้าตึงลอกเป็นขุย อาจตึงมากถึงขนาดยิ้มไม่ออก คือยิ้มแล้วผิวหน้าจะปริก็เป็นได้</p>
+                    <p>จะเห็นว่าผิวแต่ละคนไม่เหมือนกัน เครื่องสำอางที่คนหนึ่งใช้แล้วดูสวยขึ้น อีกคนนำไปใช้อาจได้ผลไม่เหมือนกัน สรุปว่าการจะเลือกใช้เครื่องสำอางให้ได้ประโยชน์ขึ้นอยู่กับเลือกชนิดที่เหมาะสมกับผิวของแต่ละคน</p>
             </div>
         </div>
 
